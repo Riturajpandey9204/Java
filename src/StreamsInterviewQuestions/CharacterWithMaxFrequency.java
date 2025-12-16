2aaa62a03f2cdf1e6b2ddb6b199416a87efde944
ritur <riturajpandey9204@gmail.com>
2025-12-16 23:55:19 +0530

    Testing

--- src/StreamsInterviewQuestions/CharacterWithMaxFrequency.java
+++ src/StreamsInterviewQuestions/CharacterWithMaxFrequency.java
@@ -1,6 +1,7 @@
 package StreamsInterviewQuestions;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -9,13 +10,19 @@ public class CharacterWithMaxFrequency {
 
         String str = "javadeveloper";
 
-        Map<Character,Long> map = str.chars().mapToObj(x->(char)x)
-                .collect(Collectors.groupingBy(x->x,Collectors.counting()));
-
-        Character ch = map.entrySet().stream().max(Map.Entry.comparingByValue())
-                .map(Map.Entry::getKey).orElse(null);
-        System.out.println(ch);
+//        Map<Character,Long> map = str.chars().mapToObj(x->(char)x)
+//                .collect(Collectors.groupingBy(x->x,Collectors.counting()));
+//
+//        Optional<Character> ch = map.entrySet().stream().max(Map.Entry.comparingByValue())
+//                .map(Map.Entry::getKey);
+//        System.out.println(ch.get());
         // https://youtu.be/7UVQmv2xZNk?list=PL63BDXJjNfTElajNCfg_2u_pbe1Xi7uTy
 
+        Map<Character,Long> map = str.chars().mapToObj(x->(char)x)
+                .collect(Collectors.groupingBy(x->x, Collectors.counting()));
+
+        Optional<Character> ch = map.entrySet().stream().max(Map.Entry.comparingByValue())
+                .map((Map.Entry::getKey));
+        System.out.println(ch.get());
     }
 }
